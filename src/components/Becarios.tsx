2a7acc01188bec4b6e6c28acad7b186b3ffe4da1
Santiago Palacio <spa497@gmail.com>
2026-09-01 23:05:43 -0300

    feat: update IEHS fellows section

--- src/components/Becarios.tsx
+++ src/components/Becarios.tsx
@@ -1,133 +1,67 @@
-/* eslint-disable @next/next/no-img-element */
-// components/Becarios.tsx
 "use client"
-import React, { useState } from "react"
 
-const becarios = [
-  { nombre: "Lic. Giuliana Castiglia", foto: "", descripcion: "" },
-  { nombre: "Lic. Luna Dobal", foto: "/LunaDobal.jpg", descripcion: "Luna Dobal es Profesora y Licenciada en Historia por la Universidad del Centro de la Provincia de Buenos Aires. Actualmente se encuentra cursando el Doctorado en Historia del Instituto de Estudios Histórico-Sociales de la Facultad de Ciencias Humanas de la Universidad Nacional del Centro de la Provincia de Buenos Aires. Es docente en institutos terciarios en la ciudad de Azul. Cuenta con una beca interna doctoral con lugar de trabajo en el Instituto de Estudios Histórico-Sociales e Instituto de Geografía, Historia y Ciencias Sociales. En su tema de investigación aborda los procesos de construcción de la ciudadanía femenina, en la zona sudeste de la provincia de Buenos Aires, desde un enfoque que dialoga con los aportes de la historia de las emociones, los estudios de género y la política." },
-  { nombre: "Lic. Juan Pablo Fosatti", foto: "/JuanFossati.jpg", descripcion: "Juan Pablo Fossati es Profesor y Licenciado en Historia por la Universidad Nacional del Centro de la Provincia de Buenos Aires. Cursa el Doctorado en Historia de la Facultad de Humanas de la mencionada casa de estudios. Es becario doctoral, con lugar de trabajo en el Instituto de Estudios Histórico-Sociales e Instituto de Geografía, Historia y Ciencias Sociales. Se desempeña como ayudante diplomado con dedicación simple de las cátedras de Historia Social General Contemporánea de la carrera de Trabajo Social y de Historia General VI de la carrera de Historia de la Facultad de Ciencias Humanas de la Universidad Nacional del Centro de la Provincia de Buenos Aires. Su línea de investigación se enfoca en analizar la dinámica política del Partido Justicialista bonaerense, esto implica examinar el comportamiento de los distintos actores que dinamizaron la vida interna del órgano partidario durante los primeros años de reconstrucción democrática, precisamente entre 1982 y 1987." },
-  { nombre: "Lic. Julia Olazábal", foto: "/JuliaOlazabal.jpeg", descripcion: "Julia Olazábal es Profesora y Licenciada en Historia por la Universidad Nacional del Centro de la Provincia de Buenos Aires. Es becaria doctoral con lugar de trabajo en el Instituto de Geografía, Historia y Ciencias Sociales. Su tema de investigación se centra en el análisis de la obra pedagógica desarrollada por Lev Tolstóy, indagando, por un lado, la forma en que dicho proyecto interactuó con las premisas educativas de la sociedad rusa entre fines del siglo XIX y comienzos del XX y, por el otro, el modo en el cual estos escritos circularon y fueron recepcionados dentro de las fronteras del Imperio Ruso, en el período comprendido entre 1860 y 1920. Para ello, analiza el rol de Tolstóy en tanto intelligent (un sujeto ocupado con el trabajo intelectual pero que asume de modo paralelo un fuerte compromiso moral), considerando no sólo sus tratados sobre educación, sino también el modo en que el autor incluía estas cuestiones en las obras literarias, desde la crítica al sistema educativo imperante hasta las justificaciones del mismo, y la forma en que pensó su contexto. Paralelamente se focaliza, entre otras cuestiones, en el tipo de educación que recibían niños y niñas en la Rusia finisecular según los roles de género establecidos, pero también, en las influencias posteriores que tuvo este proyecto en el campo de la pedagogía y en el desarrollo de la ciencia de la educación." },
-  { nombre: "Mgt. Bárbara Sosa", foto: "/BarbaraSosa.jpg", descripcion: "Bárbara Sosa es Magíster en Arte y Sociedad en Latinoamérica por la Universidad Nacional del Centro de la Provincia de Buenos Aires, Licenciada en Museología y Repositorios Culturales y Naturales por la Universidad Nacional de Avellaneda y Técnica Superior en Gestión Cultural por el Instituto de Formación Docente y Técnica N°10. Ha desarrollado numerosos proyectos vinculados al patrimonio cultural en museos y redes de instituciones culturales, a la vez que se ha desempeñado de manera profesional brindando servicios en museos públicos y privados. Su área de estudios se ha orientado al arte público, a la salvaguarda y difusión del patrimonio cultural en vinculación con las manifestaciones culturales y con procesos socio-territoriales y a la comunicación en museos y su vínculo con la memoria social. Es becaria doctoral con lugar de trabajo en el Instituto de Estudios Histórico-Sociales e Instituto de Geografía, Historia y Ciencias Sociales. Cursa el Doctorado en Historia del Instituto de Estudios Histórico-Sociales de la Facultad de Ciencias Humanas de la Universidad Nacional del Centro de la Provincia de Buenos Aires. En el marco de su beca, su investigación se orienta al patrimonio histórico material e inmaterial asociado a la explotación artesanal de la piedra en la ciudad de Tandil." },
-  { nombre: "Dra. Selene Arislur", foto: "", descripcion: "" },
-  { nombre: "Dra. María de la Paz Bidauri", foto: "/PazBidauri.jpg", descripcion: "María de la Paz Bidauri es Licenciada y Profesora de Sociología y Doctora en Ciencias Sociales por la la Universidad Nacional de La Plata. Obtuvo dos especializaciones del Ministerio de Educación en Políticas y Programas Socioeducativos y en Problemática del Mundo Contemporáneo. Actualmente cursa la Especialización y Maestría en Docencia Universitaria en la UTN en la ciudad de La Plata. Ha participado en diversos proyectos de investigación en la Universidad Nacional de La Plata dirigidos por Leticia Muñiz Terra sobre trayectorias laborales, clases sociales y desigualdad. Además ha formado parte de la Internacional Network for Comparative Social Inequalities de la Unión Europea, desarrollando estancias en el exterior. Es docente de nivel secundario y de la carrera de sociología en la Universidad Nacional de La Plata. Como profesora invitada ha dado clase en grado en la Universidad Pedagógica Nacional y en posgrado en la Universidad Nacional de Tres de Febrero y en la Universidad Nacional de Misiones.  Ha desarrollado tareas de gestión en la UTN La Plata en el acompañamiento a las trayectorias y en la Unidad de Género. Desarrolló su beca doctoral a través del Consejo Nacional de Investigaciones Científicas y Técnicas en temas estratégicos en el Centro Interdisciplinario de Metodología de las Ciencias Sociales del Instituto de Investigaciones en Humanidades y Ciencias Sociales. Actualmente se incorporó como becaria pos doctoral en el Instituto de Geografía, Historia y Ciencias Sociales. El objetivo de la beca consiste en el análisis de las trayectorias educativo-laborales de jóvenes egresadas de educación técnica en electromecánica y automotores considerando cómo inciden la socialización laboral de una ciudad intermedia como Tandil. Sus intereses de investigación son la sociología del trabajo: trayectorias laborales, programas de empleo e inserción; la sociología y economía de la educación: trayectorias educativas, modalidad de educación técnico-profesional, programas de terminalidad abordando la dinámica educación-trabajo; desigualdad social abordando dimensiones como género, edad y clase social; métodos y técnicas de Investigación: abordajes cualitativos, enfoque biográfico." },
-  { nombre: "Dra. María Soledad González", foto: "/MariaGonzalez.jpg", descripcion: "María Soledad González es Doctora en Historia por  la Universidad Nacional del Centro de la Provincia de Buenos Aires. Cuenta con una beca posdoctoral con lugar de trabajo en el Instituto de Estudios Histórico-Sociales e Instituto de Geografía, Historia y Ciencias sociales donde investiga las configuraciones y trayectorias culturales e intelectuales en el interior bonaerense a partir del abordaje de la localidad de Azul, sus vínculos regionales, nacionales e transnacionales en el periodo que comprende las décadas de 1920 a 1960. Allí profundiza sobre el rol de las mujeres ligadas a la docencia que irrumpieron en lo público en espacios de sociabilidad cultural y oficiando como destacadas gestoras culturales, editoras y escritoras." },
-];
+import { useEffect, useState } from "react"
+
+type Becario = {
+  nombre: string
+  descripcion: string
+  contacto: string
+}
+
+const becarios: Becario[] = [
+  { nombre: "Luna Dobal", descripcion: "Luna Dobal es Profesora y Licenciada en Historia por la Universidad Nacional del Centro de la Provincia de Buenos Aires, y doctoranda del Programa de Doctorado en Historia del Instituto de Estudios Histórico-Sociales (FCH-UNICEN).\n\nEs becaria posdoctoral CONICET.\n\nProyecto: Amor, mujeres y política: El amor como práctica y discurso en los procesos de construcción de la ciudadanía femenina.", contacto: "mailto:lunadobal@gmail.com" },
+  { nombre: "Juan Pablo Fossati", descripcion: "Juan Pablo Fossati es Profesor y Licenciado en Historia por la Universidad Nacional del Centro de la Provincia de Buenos Aires, y doctorando del Programa de Doctorado en Historia del Instituto de Estudios Histórico-Sociales (FCH-UNICEN).\n\nEs becario doctoral CONICET\n\nProyecto: La dinámica interna del Partido Justicialista en la provincia de Buenos Aires durante la reconstrucción democrática (1982- 1987)", contacto: "mailto:j.pfossati.jpf@gmail.com" },
+  { nombre: "Julia Olazábal", descripcion: "Julia Olazábal es Profesora y Licenciada en Historia por la Universidad Nacional del Centro de la Provincia de Buenos Aires, y doctoranda del Programa de Doctorado en Historia del Instituto de Estudios Histórico-Sociales (FCH-UNICEN).\n\nBecaria doctoral CONICET\n\nProyecto: El escritor ruso como intelligent y la literatura como teoría pedagógica. La construcción de un proyecto educativo en la obra de Lev Tolstóy (1860-1920).", contacto: "mailto:olazabaljulia@gmail.com" },
+  { nombre: "Bárbara Sosa", descripcion: "Bárbara Soca es Técnica Superior en gestión cultural, Licenciada en Museología y repositorios culturales y naturales (UNDAV) y Magister en Arte y Sociedad en Latinoamérica (Facultad de Arte- UNICEN). Doctoranda en el Programa de Doctorado en Historia del Instituto de Estudios Histórico-Sociales (FCH-UNICEN).\n\nEs becaria doctoral CONICET\n\nProyecto: Patrimonio y Comunidad. La protección del legado histórico y cultural a través de la memoria social del barrio Cerro Leones de la ciudad de Tandil, provincia de Buenos Aires.", contacto: "mailto:barbara.sosa04@gmail.com" },
+  { nombre: "Selene Arislur", descripcion: "Selene Arislur es Diplomada Universitaria Superior en Comunicación Pública (UNICEN) y Doctora en Arqueología y Licenciada en Ciencias Antropológicas con orientación Arqueológica por la Universidad de Buenos Aires.\n\nBecaria posdoctoral CONICET\n\nProyecto: Estudio comparativo multilineal de colecciones arqueológicas privadas y conjuntos de recolección superficial sistemática de sitios arqueológicos en el partido de Tandil (provincia de Buenos Aires).", contacto: "mailto:arislurselene@gmail.com" },
+  { nombre: "María de la Paz Bidauri", descripcion: "María de la Paz Bidauri es Licenciada y Profesora de Sociología y Doctora en Ciencias Sociales por la Universidad Nacional de La Plata.\n\nBecaria posdoctoral CONICET\n\nProyecto: Trayectorias educativo-laborales de mujeres técnicas. El caso de egresadas en automotores y electromecánica en Tandil (2018-2023)", contacto: "mailto:paz.bidauri86@gmail.com" },
+  { nombre: "María Soledad González", descripcion: "María Soledad González es Doctora en Historia por la Universidad Nacional del Centro de la Provincia de Buenos Aires.\n\nBecaria posdoctoral CONICET\n\nProyecto: Educadoras, gestoras y mediadoras culturales en el interior bonaerense (1960-1980)", contacto: "mailto:msgonzalez@live.com.ar" },
+]
 
 export default function Becarios() {
-  const [popupIdx, setPopupIdx] = useState<number | null>(null);
+  const [seleccionado, setSeleccionado] = useState<Becario | null>(null)
+
+  useEffect(() => {
+    if (!seleccionado) return
+    const cerrarConEscape = (event: KeyboardEvent) => {
+      if (event.key === "Escape") setSeleccionado(null)
+    }
+    document.addEventListener("keydown", cerrarConEscape)
+    return () => document.removeEventListener("keydown", cerrarConEscape)
+  }, [seleccionado])
 
   return (
-    <section className="max-w-5xl mx-auto px-4 py-12">
-      <h1
-  className="text-4xl md:text-5xl font-extrabold mb-10 text-center bg-gradient-to-r from-[#134e5e] via-[#22677e] to-[#f9623e] bg-clip-text text-transparent drop-shadow-md tracking-tight leading-relaxed"
->
-        Becarios/as
+    <section className="mx-auto max-w-5xl px-4 py-12">
+      <h1 className="text-4xl md:text-5xl font-extrabold mb-10 text-center bg-gradient-to-r from-[#134e5e] via-[#22677e] to-[#f9623e] bg-clip-text text-transparent drop-shadow-md tracking-tight leading-relaxed">
+        Becarios
       </h1>
-       <div className="grid gap-10 sm:grid-cols-2 md:grid-cols-3">
-  {becarios.map((inv, idx) => (
-    <div
-      key={inv.nombre}
-      onClick={() => setPopupIdx(idx)}
-      className="
-        flex flex-col justify-between rounded-3xl shadow-xl
-        transition-transform duration-200 hover:-translate-y-1 cursor-pointer
-        bg-white overflow-hidden min-h-[380px]
-        border border-[#134e5e]/15
-      "
-    >
-      {/* ---------- Cuerpo ---------- */}
-      <div className="flex flex-col items-center px-6 pt-10 pb-6 flex-1">
-        <img
-          src={
-            inv.foto ||
-            `https://ui-avatars.com/api/?name=${encodeURIComponent(inv.nombre)}&background=134e5e&color=fff`
-          }
-          alt={inv.nombre}
-          className="
-            w-24 h-24 rounded-full object-cover mb-4
-            border-4 border-white shadow-md ring-2 ring-[#134e5e]/15
-          "
-        />
-        <p className="text-xs text-[#134e5e] mb-1 tracking-wide uppercase">
-          Becarios/as
-        </p>
-        <h3 className="text-[1.1rem] font-semibold text-[#134e5e] text-center mb-2">
-          {inv.nombre}
-        </h3>
-        <p className="text-sm text-gray-600 text-center line-clamp-3">
-          {inv.descripcion || "—"}
-        </p>
-      </div>
-      {/* ---------- Franja inferior bicolor SIN TEXTO NI ICONOS ---------- */}
-      <div
-  className="grid grid-cols-3 text-center text-white text-[0.7rem] font-medium
-             bg-gradient-to-r from-[#134e5e] to-[#f9623e]"
->
-  <div className="py-3">&nbsp;</div>
-  <div className="py-3">&nbsp;</div>
-  <div className="py-3">&nbsp;</div>
-</div>
-
-    </div>
-  ))}
-</div>
-
-      {/* Modal centrado horizontal, textos alineados a la izquierda */}
-      {popupIdx !== null && (
-        <>
-          {/* Fondo oscuro */}
-          <div
-            className="fixed inset-0 bg-black/40 z-40"
-            onClick={() => setPopupIdx(null)}
-          />
-          {/* Modal rectangular horizontal */}
-          <div className="fixed inset-0 z-50 flex items-center justify-center">
-            <div
-        className="
-          relative flex flex-col items-center
-          rounded-[2rem] shadow-2xl border border-[#22677e]/25
-          max-w-xl w-full pt-16 pb-8 px-8
-          bg-gradient-to-br from-[#eaf3fa] via-[#f7fafc] to-[#fce4d6]
-          animate-fadein
-        "
-        style={{ minHeight: "360px" }}
-      >
-              {/* Botón cerrar */}
-              <button
-                onClick={() => setPopupIdx(null)}
-                className="absolute top-3 right-4 text-2xl text-[#134e5e] font-bold hover:text-black"
-                aria-label="Cerrar"
-              >
-                ×
-              </button>
-              <img
-                src={
-                  becarios[popupIdx].foto ||
-                  `https://ui-avatars.com/api/?name=${encodeURIComponent(
-                    becarios[popupIdx].nombre
-                  )}&background=134e5e&color=fff`
-                }
-                alt={becarios[popupIdx].nombre}
-                className="w-24 h-24 rounded-full mb-3 object-cover border-4 border-[#134e5e]/30"
-              />
-              <div className="text-lg font-bold text-[#134e5e] text-center  w-full">
-          {becarios[popupIdx].nombre}
-        </div>
-              <div className="w-16 h-1 rounded-full my-2 bg-gradient-to-r from-[#134e5e] to-[#f9623e]" />
-        <p className="text-gray-800 text-sm text-justify whitespace-pre-line w-full">
-             {becarios[popupIdx].descripcion}
-        </p>
+      <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
+        {becarios.map((becario) => (
+          <article key={becario.nombre} className="flex h-full flex-col rounded-3xl border border-[#134e5e]/15 bg-white p-6 shadow-xl transition-transform duration-200 hover:-translate-y-1">
+            <p className="mb-1 text-xs uppercase tracking-wide text-[#134e5e]">Becario/a</p>
+            <h2 className="mb-3 text-[1.1rem] font-semibold text-[#134e5e]">{becario.nombre}</h2>
+            <p className="mb-5 line-clamp-3 flex-1 whitespace-pre-line text-sm text-gray-600">{becario.descripcion}</p>
+            <div className="flex flex-wrap gap-x-4 gap-y-2 border-t border-[#134e5e]/15 pt-4 text-sm font-medium">
+              <button type="button" onClick={() => setSeleccionado(becario)} className="text-[#134e5e] underline-offset-4 hover:text-[#f9623e] hover:underline focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#f9623e]">Ver perfil</button>
+              <a href={becario.contacto} className="text-[#134e5e] underline-offset-4 hover:text-[#f9623e] hover:underline focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#f9623e]">Contacto</a>
             </div>
+          </article>
+        ))}
+      </div>
+
+      {seleccionado && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto p-4">
+          <button type="button" aria-label="Cerrar perfil" className="absolute inset-0 cursor-default bg-black/40" onClick={() => setSeleccionado(null)} />
+          <div role="dialog" aria-modal="true" aria-labelledby="modal-becario-titulo" className="relative z-10 max-h-[calc(100vh-2rem)] w-full max-w-xl overflow-y-auto rounded-[2rem] border border-[#22677e]/25 bg-gradient-to-br from-[#eaf3fa] via-[#f7fafc] to-[#fce4d6] px-6 py-8 shadow-2xl animate-fadein sm:px-8 sm:py-10">
+            <button type="button" onClick={() => setSeleccionado(null)} className="absolute right-4 top-3 text-2xl font-bold text-[#134e5e] hover:text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#f9623e]" aria-label="Cerrar perfil">×</button>
+            <h2 id="modal-becario-titulo" className="pr-8 text-center text-lg font-bold text-[#134e5e]">{seleccionado.nombre}</h2>
+            <div className="my-3 h-1 w-16 rounded-full bg-gradient-to-r from-[#134e5e] to-[#f9623e]" />
+            <p className="whitespace-pre-line text-justify text-sm text-gray-800">{seleccionado.descripcion}</p>
+            <a href={seleccionado.contacto} className="mt-6 inline-block text-sm font-medium text-[#134e5e] underline-offset-4 hover:text-[#f9623e] hover:underline focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#f9623e]">Contacto</a>
           </div>
-        </>
+        </div>
       )}
-      <style>
-        {`
-          @keyframes fadein { from { opacity: 0; transform: scale(0.96); } to { opacity: 1; transform: scale(1); } }
-          .animate-fadein { animation: fadein 0.2s ease; }
-        `}
-      </style>
+      <style>{`@keyframes fadein { from { opacity: 0; transform: scale(0.96); } to { opacity: 1; transform: scale(1); } } .animate-fadein { animation: fadein 0.2s ease; }`}</style>
     </section>
-  );
+  )
 }
